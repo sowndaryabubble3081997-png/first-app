ffb7d558c76f667e30d3e75553ba418b0d9bac86
sowndarya rajkumar <sowndaryabubble3081997@gmail.com>
2026-02-04 12:40:27 +0530

    Initial commit

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+   
+    export default function Header() {
+        
+        console.log("header")
+      return (
+        <div>
+          <h1 className=' bg-pink-800 hover:bg-violet-700'>E-Commerece Product Dashboard</h1>
+
+        </div>
+      )
+    }
+    
---
+++ main.jsx
@@ -0,0 +1,10 @@
+import react from 'react'
+import ReactDom from 'react-dom/client'
+import './App.css'
+import App from './App.jsx'
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  
+    <App />
+  
+)
